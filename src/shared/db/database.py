11a023ae9860cccd4a0380dedacd469ab59c85a5
bchanona <bh274266@gmail.com>
2025-08-12 15:44:10 -0600

    feat(api): Add connection to the database

---
+++ src/shared/db/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+DATABASE_URL = os.getenv("POSTGRESURL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
