985f973cbf45a991a0ac4208c517c52c73c3c81c
bchanona <bh274266@gmail.com>
2025-08-14 14:25:49 -0600

    feat(api): add models create subject

---
+++ src/app/asignature/domain/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, field_validator, ValidationInfo
+
+
+class CreateAsignatureModel(BaseModel):
+    idTeacher: int
+    name: str
+    description: str
+
+    @field_validator('name')
+    def name_must_not_be_empty(cls, value, info: ValidationInfo):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        return value
+
+    @field_validator('description')
+    def description_must_not_be_empty(cls, value, info: ValidationInfo):
+        if not value:
+            raise ValueError("Description cannot be empty")
+        return value
+
+
+class CreateResponse(BaseModel):
+    name: str
+    description: str
