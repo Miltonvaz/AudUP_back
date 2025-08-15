601ba808001811d18c4e9d0f3486af0a9e520e6a
Miltonvaz <233427@ids.upchiapas.edu.mx>
2025-08-14 19:21:16 -0600

    User module completed

--- src/app/user/infrastructure/controllers/getAll.py
+++ src/app/user/infrastructure/controllers/getAll.py
@@ -1,7 +1,6 @@
-# getAll.py - Controller corregido
 from typing import List
 from src.app.user.domain.entities.models import CreateUserResponse
-from src.app.user.application.use_case.getAll_user import GetAllUser  # <-- Use Case
+from src.app.user.application.use_case.getAll_user import GetAllUser  
 
 class GetAllUsersController:
     def __init__(self, usecase: GetAllUser):
