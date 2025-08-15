424f807a8ad230c985bbe8467a160fa8eef48c41
bchanona <bh274266@gmail.com>
2025-08-15 14:14:57 -0600

    fix(api): correcting auth.py

--- src/shared/security/auth.py
+++ src/shared/security/auth.py
@@ -2,23 +2,29 @@ import jwt
 from datetime import datetime, timedelta
 from typing import Optional
 from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
 
-JWT_SECRET = "mi_clave_secreta"
-JWT_ALGORITHM = "HS256"
-JWT_EXPIRE_HOURS = 24
+load_dotenv()
+
+JWT_SECRET = os.getenv("SECRET_KEY")
+JWT_ALGORITHM = os.getenv("ALGORITHM")
+JWT_EXPIRE_HOURS = int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS"))  
 
 
 class Claims(BaseModel):
     user_id: int
     email: str
+    role: str
     exp: Optional[int] = None  
 
 
-def generate_jwt(user_id: int, email: str) -> str:
+def generate_jwt(user_id: int, email: str, role : str) -> str:
     expiration_time = datetime.utcnow() + timedelta(hours=JWT_EXPIRE_HOURS)
     payload = {
         "user_id": user_id,
         "email": email,
+        "role": role, 
         "exp": int(expiration_time.timestamp()) 
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
@@ -31,6 +37,7 @@ def validate_jwt(token: str) -> Claims:
         return Claims(
             user_id=payload["user_id"],
             email=payload["email"],
+            role=payload["role"], 
             exp=payload.get("exp")
         )
     except jwt.ExpiredSignatureError:
