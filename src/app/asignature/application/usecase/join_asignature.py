08c35482f6a655af0da05deb5aa1aaab20982ae1
bchanona <bh274266@gmail.com>
2025-08-21 17:33:38 -0600

    fix(api): Correct usecase

--- src/app/asignature/application/usecase/join_asignature.py
+++ src/app/asignature/application/usecase/join_asignature.py
@@ -4,10 +4,11 @@ class JoinAsignature:
     def __init__(self, repo: AsignatureRepository):
         self.repo = repo
         
-    def execute(self, user_id: int, asignature_id: int) -> None:
-        if not self.repo.exists_asignature(user_id, asignature_id):
-            raise ValueError("Asignature not found")
+    def execute(self, user_id: int, asignature_id: int) -> str:
         
-        success = self.repo.join_asignature(user_id, asignature_id)
-        if not success:
+        result = self.repo.join_asignature(user_id, asignature_id)
+        
+        if not result:
             raise ValueError("Could not join asignature")
+        
+        return result
