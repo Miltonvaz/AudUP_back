d662067da013295520541165544cadab11340929
bchanona <bh274266@gmail.com>
2025-08-21 18:50:30 -0600

    fix(api): Correct usecase

--- src/app/asignature/application/usecase/update_background.py
+++ src/app/asignature/application/usecase/update_background.py
@@ -1,13 +1,17 @@
 from src.app.asignature.domain.repository import AsignatureRepository
 
+
 class UpdateBackground:
     def __init__(self, repo: AsignatureRepository):
         self.repo = repo
 
     async def execute(self, asignature_id: int, background_url: str, user_id: int):
-        updated_asignature = self.repo.update_background(asignature_id, background_url, user_id)
+        if not self.repo.exists_asignature(user_id, asignature_id):
+            raise ValueError("Asignature not found")
 
-        
+        updated_asignature = self.repo.update_background(
+            asignature_id, background_url, user_id)
+
         return {
             "name": updated_asignature.name,
             "background_url": updated_asignature.urlBackground
